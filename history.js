4fd22d1c2e8a707ead5a790a4417d7f8e231297b
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-05 14:53:24 +0200

    WIP code for viewing all history.

---
+++ history.js
@@ -0,0 +1,68 @@
+var baseUrl = "http://localhost:8080/travel-history/";
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+/**
+ * Converts a day of the month into an ordinal (e.g. 1st, 2nd, 3rd). Needs to be improved.
+ */
+function asOrdinal(day){
+	var ending;
+	if(day in [1,21,31]) {ending = "st";}
+	else if(day in [2,22]) {ending = "nd";}
+	else if(day in [3,23]) {ending = "rd";}
+	else {ending = "th";}
+	return day + ending;
+}
+
+/**
+ * Combines the startDate and endDate timestamps. Dates use one of three forms, based on relevant info:
+ * April 21st onwards
+ * April 21st - 28th
+ * April 21st - May 2nd
+ */
+function getCombinedTimestamp(startDate, endDate) {
+	if(endDate == null){
+		return asDate(startDate) + " onwards";
+	} else if(startDate.monthOfYear == endDate.monthOfYear) {
+		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + asOrdinal(endDate.dayOfMonth);
+	} else {
+		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + months[endDate.monthOfYear] + asOrdinal(endDate.dayOfMonth);
+	}		
+}
+
+/**
+ * A single trip.
+ */
+function Trip(name, country, startDate, endDate, blog, mapUrl){
+	var self = this;
+	
+	self.name = name;
+	self.country = country;
+	self.combinedTimestamp = getCombinedTimestamp(startDate, endDate);
+	self.blogUrl = blog != null ? blog.url : null;
+	self.blogName = blog != null ? blog.name : null;
+	self.mapUrl = mapUrl;
+}
+
+/**
+ * Main Knockout view model containing the trip history.
+ */
+function HistoryViewModel(){
+	var self = this;
+	
+	self.trips = ko.observableArray([]);
+	
+	$.get(baseUrl + "history", function(data){
+		console.log(data);
+		self.trips($.map(data, function(item){
+			return new Trip(item.name, item.country, item.startTime, item.endTime, item.blog, item.mapUrl);
+		}));
+	})
+}
+
+/**
+ * Register the HistoryViewModel.
+ */
+$( document ).ready(function() {
+	ko.applyBindings(new HistoryViewModel());
+});
