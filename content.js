22c9a16faab005f2246fee08978e6c16e0b102e9
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-06 15:48:00 +0200

    Updated the main page to use knockout, and moved the main binding into content, rather than history.

--- content.js
+++ content.js
@@ -1,29 +1,101 @@
-var baseUrl = "http://54.191.146.40:8080/travel-history/";
+var baseUrl = "http://localhost:8080/travel-history/";
 
-function getLatestBlogPost(){
-	$.get(baseUrl + "blog/latest", function(data){
-		$('#latest-blog-post').attr('href', data.url);
-		$('#latest-blog-post').text(data.name);
-	})
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+/**
+ * Converts a day of the month into an ordinal (e.g. 1st, 2nd, 3rd).
+ */
+function asOrdinal(day){
+	switch(day){
+		case 1:
+		case 21: 
+		case 31:
+			return day + "st";
+			break;
+		case 2: 
+		case 22:
+			return day + "nd";
+			break;
+		case 3: 
+		case 23:
+			return day + "rd";
+			break;
+		default:
+			return day + "th";
+			break;
+	}
 }
 
-function getCurrentLocation(){
+/**
+ * Combines the startDate and endDate timestamps. Dates use one of three forms, based on relevant info:
+ * April 21st onwards
+ * April 21st - 28th
+ * April 21st - May 2nd
+ */
+function getCombinedTimestamp(startDate, endDate) {
+	if(endDate == null){
+		return asDate(startDate) + " onwards";
+	} else if(startDate.monthOfYear == endDate.monthOfYear) {
+		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + asOrdinal(endDate.dayOfMonth);
+	} else {
+		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + months[endDate.monthOfYear] + " " + asOrdinal(endDate.dayOfMonth);
+	}		
+}
+
+/**
+ * Knockout Data class for a single trip.
+ */
+function Trip(name, country, startDate, endDate, timezone, blog, mapUrl){
+	var self = this;
+	
+	self.name = name;
+	self.country = country;
+	self.startPoint = startDate.millis;
+	self.timezone = timezone >= 0 ? "+" + timezone : timezone;
+	self.combinedTimestamp = getCombinedTimestamp(startDate, endDate);
+	self.blog = blog != null ? new Blog(blog.name, blog.url) : null;
+	self.mapUrl = mapUrl;
+}
+
+/**
+ * Knockout Data class for a blog.
+ */
+function Blog(name, url){
+	var self = this;
+	self.name = name;
+	self.url = url;
+}
+
+/**
+ * Main Knockout view model containing the trip history.
+ */
+function ContentViewModel(){
+	var self = this;
+	
+	self.latestBlog = ko.observable();
+	self.currentTrip = ko.observable();
+	self.trips = ko.observableArray([]);
+	
 	$.get(baseUrl + "history/current", function(data){
-		$('#location-map').attr('href', data.mapUrl);
-		$('#location-map > img').attr('src', data.mapUrl);
-		
-		$('#location-name').text(data.name);	
-		$('#location-country').text(data.country);
-		
-		if(data.timezone >= 0){
-			$('#location-timezone').text("UTC +" +data.timezone);
-		} else {
-			$('#location-timezone').text("UTC " +data.timezone);
-		}
+		self.currentTrip(new Trip(data.name, data.country, data.startTime, data.endTime, data.timezone, data.blog, data.mapUrl));
+	})
+	
+	$.get(baseUrl + "blog/latest", function(data){
+		self.latestBlog(new Blog(data.name, data.url));
+	})
+	
+	$.get(baseUrl + "history", function(data){
+		self.trips($.map(data, function(item){
+			return new Trip(item.name, item.country, item.startTime, item.endTime, item.timezone, item.blog, item.mapUrl);
+		}).sort(function(a, b) {
+			return b.startPoint - a.startPoint;
+		}));
 	})
 }
 
-$(function(){
-	getLatestBlogPost();
-	getCurrentLocation();
-})
+/**
+ * Register the ContentViewModel.
+ */
+$( document ).ready(function() {
+	ko.applyBindings(new ContentViewModel());
+});
