0207c36ec25863a0672ded27e9e9e625e3f9c368
William Bradshaw <w.a.bradshaw@gmail.com>
2018-08-08 17:58:19 +0200

    Fixed a bug where places without an end date would break (because I forgot to rename a function)

--- content.js
+++ content.js
@@ -34,7 +34,7 @@ function asOrdinal(day){
  */
 function getCombinedTimestamp(startDate, endDate) {
 	if(endDate == null){
-		return asDate(startDate) + " onwards";
+		return asOrdinal(startDate) + " onwards";
 	} else if(startDate.monthOfYear == endDate.monthOfYear) {
 		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + asOrdinal(endDate.dayOfMonth);
 	} else {
