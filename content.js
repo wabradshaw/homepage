45498787a8899611f1847f1e22369b24ceb58f08
William Bradshaw <w.a.bradshaw@gmail.com>
2023-09-11 11:30:04 +0100

    Switched to local location history and dropped dates

--- content.js
+++ content.js
@@ -1,77 +1,24 @@
 var baseUrl = "https://wabradshaw.com/travel-history/";
 
-var months = ["December", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-/**
- * Converts a day of the month into an ordinal (e.g. 1st, 2nd, 3rd).
- */
-function asOrdinal(day){
-	switch(day){
-		case 1:
-		case 21: 
-		case 31:
-			return day + "st";
-			break;
-		case 2: 
-		case 22:
-			return day + "nd";
-			break;
-		case 3: 
-		case 23:
-			return day + "rd";
-			break;
-		default:
-			return day + "th";
-			break;
-	}
-}
-
-/**
- * Combines the startDate and endDate timestamps. Dates use one of three forms, based on relevant info:
- * April 21st onwards
- * April 21st - May 2nd
- * April 21st - 28th
- * Aprin 21st
- */
-function getCombinedTimestamp(startDate, endDate) {
-	if(endDate == null){
-		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " onwards";
-	} else if(startDate.monthOfYear != endDate.monthOfYear) {
-		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + months[endDate.monthOfYear] + " " + asOrdinal(endDate.dayOfMonth);
-	} else if(startDate.dayOfMonth != endDate.dayOfMonth){
-		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth) + " - " + asOrdinal(endDate.dayOfMonth);	
-	} else {
-		return months[startDate.monthOfYear] + " " + asOrdinal(startDate.dayOfMonth);
-	}	
-}
-
 /**
  *Knockout Data class for a trip spanning multiple locations.
  */
-function Trip(name){
+function Trip(name, locations){
 	var self = this;
 
 	self.name = name;
-	self.locations = ko.observableArray([]);
-	
-	self.addLocation = function(location){
-		self.locations.push(location);
-	}
+	self.locations = ko.observableArray(locations);
 }
 
 /**
  * Knockout Data class for a single location.
  */
-function Location(uuid, name, country, trip, startDate, endDate, timezone, blog, mapUrl){
+function Location(uuid, name, country, mapUrl){
 	var self = this;
 
 	self.uuid = uuid;	
 	self.name = name;
 	self.country = country;
-	self.trip = trip;
-	self.startPoint = startDate.millis;
-	self.timezone = timezone >= 0 ? "+" + timezone : timezone;
-	self.combinedTimestamp = getCombinedTimestamp(startDate, endDate);
 	self.mapUrl = mapUrl;
 }
 
@@ -82,7 +29,6 @@ function ContentViewModel(){
 	var self = this;
 	
 	self.mode = ko.observable('default');
-	self.currentLocation = ko.observable();
 	self.trips = ko.observableArray([]);
 	self.locations = ko.observableArray([]);
 	self.locationsLoaded = ko.observable(false);
@@ -100,34 +46,13 @@ function ContentViewModel(){
 			icons: false
 		});
 	}
-	
-	$.get(baseUrl + "history/current", function(data){
-		self.currentLocation(new Location(data.uuid, data.name, data.country, data.group, data.startTime, data.endTime, data.timezone, data.blog, data.mapUrl));
-	})
-		
-	$.get(baseUrl + "history", function(data){
-		self.locations($.map(data, function(item){
-			return new Location(item.uuid, item.name, item.country, item.group, item.startTime, item.endTime, item.timezone, item.blog, item.mapUrl);
-		}).sort(function(a, b) {
-			return b.startPoint - a.startPoint;
-		}));
-		
-		if(self.locations().length > 0){
-			var currentTrip = new Trip(self.locations()[0].trip);
-			$.each(self.locations(), function(index){
-				var item = self.locations()[index];
-				if(currentTrip.name != item.trip){
-					self.trips.push(currentTrip);
-					currentTrip = new Trip(item.trip);
-				}
-				currentTrip.addLocation(item);
-			})
-			self.trips.push(currentTrip);
-		}
-		
-		self.locationsLoaded(true);
-		addTravelsScene();
-	})
+
+	for (trip_name in location_data){		
+		var trip_locations = location_data[trip_name].map(item => new Location(item.uuid, item.name, item.country, item.mapUrl));
+		self.trips.push(new Trip(trip_name, trip_locations));
+	}
+
+	self.locationsLoaded(true);
 	
 	self.getTripLocations = function(targetName){		
 		return self.trips().find( obj => { return obj.name == targetName}).locations;
@@ -140,9 +65,9 @@ function toggleLocation(e){
 		loc.find('.show-trip-button > i').toggleClass('flipped');
 		loc.find('.expand-spacer').slideToggle('100');
 		loc.find('.full-history').slideToggle('1000');
-		
 	}
 }
+
 /**
  * Register the ContentViewModel.
  */
---
+++ locations.js
@@ -0,0 +1,426 @@
+location_data = {
+    "Sprinting Across Europe": [
+        {
+            "uuid": 2,
+            "name": "Faro",
+            "country": "Portugal",
+            "mapUrl": "maps/faro.png"
+        },
+        {
+            "uuid": 3,
+            "name": "Seville",
+            "country": "Spain",
+            "mapUrl": "maps/seville.png"
+        },
+        {
+            "uuid": 4,
+            "name": "Cordoba",
+            "country": "Spain",
+            "mapUrl": "maps/cordoba.png"
+        },
+        {
+            "uuid": 5,
+            "name": "Madrid",
+            "country": "Spain",
+            "mapUrl": "maps/madrid.png"
+        },
+        {
+            "uuid": 6,
+            "name": "Barcelona",
+            "country": "Spain",
+            "mapUrl": "maps/barcelona.png"
+        },
+        {
+            "uuid": 7,
+            "name": "Perpignan",
+            "country": "France",
+            "mapUrl": "maps/perpignan.png"
+        },
+        {
+            "uuid": 8,
+            "name": "Lyon",
+            "country": "France",
+            "mapUrl": "maps/lyon.png"
+        },
+        {
+            "uuid": 9,
+            "name": "Zurich",
+            "country": "Switzerland",
+            "mapUrl": "maps/hedingen.png"
+        },
+        {
+            "uuid": 10,
+            "name": "Milan",
+            "country": "Italy",
+            "mapUrl": "maps/milan.png"
+        },
+        {
+            "uuid": 11,
+            "name": "Florence",
+            "country": "Italy",
+            "mapUrl": "maps/florence.png"
+        },
+        {
+            "uuid": 12,
+            "name": "Rome",
+            "country": "Italy",
+            "mapUrl": "maps/rome.png"
+        },
+        {
+            "uuid": 13,
+            "name": "Salerno",
+            "country": "Italy",
+            "mapUrl": "maps/salerno.png"
+        },
+        {
+            "uuid": 14,
+            "name": "Vienna",
+            "country": "Austria",
+            "mapUrl": "maps/vienna.png"
+        },
+        {
+            "uuid": 15,
+            "name": "Salzburg",
+            "country": "Austria",
+            "mapUrl": "maps/salzburg.png"
+        },
+        {
+            "uuid": 16,
+            "name": "Bratislava",
+            "country": "Slovakia",
+            "mapUrl": "maps/bratislava.png"
+        },
+        {
+            "uuid": 17,
+            "name": "Budapest",
+            "country": "Hungary",
+            "mapUrl": "maps/budapest.png"
+        }
+    ],
+    "The Balkans": [
+        {
+            "uuid": 19,
+            "name": "Split",
+            "country": "Croatia",
+            "mapUrl": "maps/split.png"
+        },
+        {
+            "uuid": 20,
+            "name": "Jesenice",
+            "country": "Croatia",
+            "mapUrl": "maps/jesenice.png"
+        },
+        {
+            "uuid": 21,
+            "name": "Mostar",
+            "country": "Bosnia and Herzegovina",
+            "mapUrl": "maps/mostar.png"
+        },
+        {
+            "uuid": 22,
+            "name": "Dubrovnik",
+            "country": "Croatia",
+            "mapUrl": "maps/dubrovnik.png"
+        },
+        {
+            "uuid": 23,
+            "name": "Kotor",
+            "country": "Montenegro",
+            "mapUrl": "maps/kotor.png"
+        },
+        {
+            "uuid": 24,
+            "name": "Budva",
+            "country": "Montenegro",
+            "mapUrl": "maps/budva.png"
+        },
+        {
+            "uuid": 25,
+            "name": "Podgorica",
+            "country": "Montenegro",
+            "mapUrl": "maps/podgorica.png"
+        },
+        {
+            "uuid": 26,
+            "name": "Tirana",
+            "country": "Albania",
+            "mapUrl": "maps/tirana.png"
+        },
+        {
+            "uuid": 27,
+            "name": "Saranda",
+            "country": "Albania",
+            "mapUrl": "maps/saranda.png"
+        },
+        {
+            "uuid": 28,
+            "name": "Berat",
+            "country": "Albania",
+            "mapUrl": "maps/berat.png"
+        },
+        {
+            "uuid": 29,
+            "name": "Tirana",
+            "country": "Albania",
+            "mapUrl": "maps/tirana.png"
+        },
+        {
+            "uuid": 30,
+            "name": "Lake Ohrid",
+            "country": "Macedonia",
+            "mapUrl": "maps/ohrid.png"
+        },
+        {
+            "uuid": 31,
+            "name": "Skopje",
+            "country": "Macedonia",
+            "mapUrl": "maps/skopja.png"
+        },
+        {
+            "uuid": 32,
+            "name": "Thessaloniki",
+            "country": "Greece",
+            "mapUrl": "maps/thessaloniki.png"
+        }
+    ],
+    "Intermission 2: The MacIntyre-Tickner Wedding": [
+        {
+            "uuid": 35,
+            "name": "Prague",
+            "country": "Czechia",
+            "mapUrl": "maps/prague.png"
+        }
+    ],
+    "Arria": [
+        {
+            "uuid": 45,
+            "name": "Bangalore",
+            "country": "India",
+            "mapUrl": "maps/bangalore.png"
+        },
+        {
+            "uuid": 50,
+            "name": "New Delhi",
+            "country": "India",
+            "mapUrl": "maps/noida.png"
+        },
+        {
+            "uuid": 51,
+            "name": "Taj Mahal",
+            "country": "India",
+            "mapUrl": "maps/agra.png"
+        }
+    ],
+    "Scandinavipalooza": [
+        {
+            "uuid": 53,
+            "name": "Riga",
+            "country": "Latvia",
+            "mapUrl": "maps/riga.png"
+        },
+        {
+            "uuid": 54,
+            "name": "Elva",
+            "country": "Estonia",
+            "mapUrl": "maps/elva.png"
+        },
+        {
+            "uuid": 55,
+            "name": "Tallinn",
+            "country": "Estonia",
+            "mapUrl": "maps/tallinn.png"
+        },
+        {
+            "uuid": 56,
+            "name": "Helsinki",
+            "country": "Finland",
+            "mapUrl": "maps/helsinki.png"
+        },
+        {
+            "uuid": 57,
+            "name": "Stockholm",
+            "country": "Sweden",
+            "mapUrl": "maps/stockholm.png"
+        },
+        {
+            "uuid": 58,
+            "name": "Karlstad",
+            "country": "Sweden",
+            "mapUrl": "maps/karlstad.png"
+        },
+        {
+            "uuid": 59,
+            "name": "Gothenburg",
+            "country": "Sweden",
+            "mapUrl": "maps/gothenburg.png"
+        }
+    ],
+    "More Arria": [
+        {
+            "uuid": 61,
+            "name": "Sydney",
+            "country": "Australia",
+            "mapUrl": "maps/sydney.png"
+        },
+        {
+            "uuid": 65,
+            "name": "Toronto",
+            "country": "Canada",
+            "mapUrl": "maps/toronto.png"
+        },
+        {
+            "uuid": 66,
+            "name": "Niagra Falls",
+            "country": "Canada",
+            "mapUrl": "maps/niagra.png"
+        }
+    ],
+    "The Farewell Tour": [
+        {
+            "uuid": 70,
+            "name": "San Francisco",
+            "country": "California",
+            "mapUrl": "maps/san-fran.png"
+        },
+        {
+            "uuid": 71,
+            "name": "Morristown",
+            "country": "New Jersey",
+            "mapUrl": "maps/morristown.png"
+        },
+        {
+            "uuid": 72,
+            "name": "Manhattan",
+            "country": "New York",
+            "mapUrl": "maps/new-york.png"
+        },
+        {
+            "uuid": 73,
+            "name": "Morristown",
+            "country": "New Jersey",
+            "mapUrl": "maps/morristown.png"
+        }
+    ],
+    "South East Asia": [
+        {
+            "uuid": 77,
+            "name": "Bangkok",
+            "country": "Thailand",
+            "mapUrl": "maps/bangkok.png"
+        },
+        {
+            "uuid": 78,
+            "name": "Chiang Mai",
+            "country": "Thailand",
+            "mapUrl": "maps/chiang-mai.png"
+        },
+        {
+            "uuid": 79,
+            "name": "Chiang Rai",
+            "country": "Thailand",
+            "mapUrl": "maps/chiang-rai.png"
+        },
+        {
+            "uuid": 80,
+            "name": "Maeklang Elephant Conservation Park",
+            "country": "Thailand",
+            "mapUrl": "maps/maeklang.png"
+        },
+        {
+            "uuid": 81,
+            "name": "Mekong River Slow Boat",
+            "country": "Laos",
+            "mapUrl": "maps/mekong.png"
+        },
+        {
+            "uuid": 82,
+            "name": "Luang Prabang",
+            "country": "Laos",
+            "mapUrl": "maps/luang-prabang.png"
+        },
+        {
+            "uuid": 83,
+            "name": "Vang Vieng",
+            "country": "Laos",
+            "mapUrl": "maps/vang-vieng.png"
+        },
+        {
+            "uuid": 84,
+            "name": "Vientiane",
+            "country": "Laos",
+            "mapUrl": "maps/vientiane.png"
+        },
+        {
+            "uuid": 85,
+            "name": "Hanoi",
+            "country": "Vietnam",
+            "mapUrl": "maps/hanoi.png"
+        },
+        {
+            "uuid": 86,
+            "name": "Sapa",
+            "country": "Vietnam",
+            "mapUrl": "maps/sapa.png"
+        },
+        {
+            "uuid": 87,
+            "name": "Hanoi",
+            "country": "Vietnam",
+            "mapUrl": "maps/hanoi.png"
+        },
+        {
+            "uuid": 88,
+            "name": "Ha Long Bay",
+            "country": "Vietnam",
+            "mapUrl": "maps/ha-long.png"
+        },
+        {
+            "uuid": 89,
+            "name": "Hanoi",
+            "country": "Vietnam",
+            "mapUrl": "maps/hanoi.png"
+        },
+        {
+            "uuid": 90,
+            "name": "Ho Chi Minh City",
+            "country": "Vietnam",
+            "mapUrl": "maps/ho-chi-minh.png"
+        },
+        {
+            "uuid": 91,
+            "name": "Phnom Penh",
+            "country": "Cambodia",
+            "mapUrl": "maps/phnom-penh.png"
+        },
+        {
+            "uuid": 92,
+            "name": "Sihanoukville",
+            "country": "Cambodia",
+            "mapUrl": "maps/sihanoukville.png"
+        },
+        {
+            "uuid": 93,
+            "name": "Koh Rong",
+            "country": "Cambodia",
+            "mapUrl": "maps/koh-rong.png"
+        },
+        {
+            "uuid": 94,
+            "name": "Phnom Penh",
+            "country": "Cambodia",
+            "mapUrl": "maps/phnom-penh.png"
+        },
+        {
+            "uuid": 95,
+            "name": "Siem Reap",
+            "country": "Cambodia",
+            "mapUrl": "maps/siem-reap.png"
+        },
+        {
+            "uuid": 96,
+            "name": "Bangkok",
+            "country": "Thailand",
+            "mapUrl": "maps/bangkok.png"
+        }
+    ]
+}
